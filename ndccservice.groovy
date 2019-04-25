14cb2fc0de4f525c337e4329fa9dc1dd4241367a
ndccroyals <49049612+ndccroyals@users.noreply.github.com>
2019-04-24 23:39:11 -0500

    Update ndccservice.groovy

--- ndccservice.groovy
+++ ndccservice.groovy
@@ -4,7 +4,7 @@ node {
 	
    stage('Preparation') { // for display purposes
       // Get some code from a GitHub repository
-      git 'https://github.com/ndccroyals/ndccservice-login-pipeline.git'
+      git 'https://github.com/ndccroyals/ndcc-service-login.git'
       // Get the Maven tool.
       // ** NOTE: This 'M3' Maven tool must be configured
       // **       in the global configuration.           
